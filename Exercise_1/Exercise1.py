9898a0ea33d1ea6ff208b926698fd024c3e40a2b
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2019-08-02 13:05:50 +0300

    Exercise 1

--- Exercise_1/Exercise1.py
+++ Exercise_1/Exercise1.py
@@ -5,3 +5,23 @@ Created on Fri Aug  2 11:19:59 2019
 @author: GHOST
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+
+
+test = []
+data= np.loadtxt('locationData.csv')
+for i in data:
+    test.append(i)
+    test.append('\n')
+    
+'''np.shape(data)
+print(data)
+plt.plot(data[:,0],data[:,1])
+plt.show()
+ax = plt.subplot(1,1,1,projection = '3d')
+ax.plot(data[:,0],data[:,1],data[:,2])
+plt.show()
+'''
