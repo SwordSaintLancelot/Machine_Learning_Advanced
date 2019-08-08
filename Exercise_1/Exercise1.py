26707afae3f0bc7f6686909cdb4493c8518a7a38
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2019-08-08 15:03:57 +0300

    Exercise 1

--- Exercise_1/Exercise1.py
+++ Exercise_1/Exercise1.py
@@ -11,12 +11,16 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import get_test_data
 
 
-test = []
+'''test = []
 data= np.loadtxt('locationData.csv')
 for i in data:
     test.append(i)
     test.append('\n')
+
+test = np.array(test)
     
+    
+print(np.shape(test))'''
 '''np.shape(data)
 print(data)
 plt.plot(data[:,0],data[:,1])
@@ -25,3 +29,32 @@ ax = plt.subplot(1,1,1,projection = '3d')
 ax.plot(data[:,0],data[:,1],data[:,2])
 plt.show()
 '''
+from scipy.io import loadmat
+
+mat = loadmat("twoClassData.mat")
+
+
+
+
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+
+def main():
+    test_list = []
+    a = np.loadtxt('locationData.csv')
+    with open("locationData.csv","r") as fd:
+        for line in fd:
+            value = line.split(" ")
+            value = [float(v) for v in value]
+            test_list.append(value)
+        test_list = np.array(test_list)    
+    print(np.shape(test_list))
+    print(a)
+    print("just checking")
+    print(test_list)
+    print(np.all(a == test_list))
+main()
