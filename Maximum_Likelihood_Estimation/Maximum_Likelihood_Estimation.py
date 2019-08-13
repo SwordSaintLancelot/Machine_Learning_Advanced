4e4e507a93a8418c24590ad31419a4c09578ac3b
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2019-08-13 13:23:12 +0300

    Check in

--- Maximum_Likelihood_Estimation/Maximum_Likelihood_Estimation.py
+++ Maximum_Likelihood_Estimation/Maximum_Likelihood_Estimation.py
@@ -8,13 +8,13 @@ Created on Fri Aug  9 14:04:28 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#Exercise 2 question 4
 n = np.arange(100)
 w = np.sqrt(0.25)*np.random.randn(100)
 x = np.sin(2*3.14*0.017*n)+w
 plt.plot(n,x)
 
-
+# Estimating the frequency of first sine wave.
 scores = [] 
 frequencies = []
 for f in np.linspace(0, 0.5, 1000):
@@ -30,26 +30,6 @@ for f in np.linspace(0, 0.5, 1000):
 fHat = frequencies[np.argmax(scores)]
 
 
-
-#%%
-
-import numpy as np
-import matplotlib.pyplot as plt
+#Exercise 4 question 5
 
 
-
-w = np.sqrt(0.25) * np.random.randn(100)
-n = np.arange(100)
-x  = np.sin(2*np.pi*.017*n) + w
-plt.plot(n,x)
-plt.show()
-scores = []
-frequencies = []
-for f in np.linspace(0, 0.5, 1000):
-# Create vector e. Assume data is in x.
-    z = -2* np.pi * f *1j * n
-    e = np.exp(z)
-    score = np.abs(np.dot(x,e))
-    scores.append(score)
-    frequencies.append(f)
-fHat = frequencies[np.argmax(scores)]
