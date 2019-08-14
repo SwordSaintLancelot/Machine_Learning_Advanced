348d56a611b8bfd28103166b0311a2262fef6743
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2019-08-14 11:24:06 +0300

    Exercise 2 complete

--- Maximum_Likelihood_Estimation/Maximum_Likelihood_Estimation.py
+++ Maximum_Likelihood_Estimation/Maximum_Likelihood_Estimation.py
@@ -7,6 +7,7 @@ Created on Fri Aug  9 14:04:28 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import imread
 
 #Exercise 2 question 4
 n = np.arange(100)
@@ -29,7 +30,41 @@ for f in np.linspace(0, 0.5, 1000):
     
 fHat = frequencies[np.argmax(scores)]
 
-
 #Exercise 4 question 5
 
+
+# Read the data
+
+img = imread("uneven.jpg")
+plt.imshow(img, cmap='gray')
+plt.title("Image shape is %dx%d" % (img.shape[1], img.shape[0]))
+plt.show()
+
+# Create the X-Y coordinate pairs in a matrix
+X, Y = np.meshgrid(range(1300), range(1030))
+Z = img
+
+x = X.ravel()
+y = Y.ravel()
+z = Z.ravel()
+
+# ********* TODO 1 **********
+# Create data matrix
+# Use function "np.column_stack".
+# Function "np.ones_like" creates a vector like the input.
+H = np.column_stack([x**2,y**2,x*y,x,y,np.ones_like(x)])
+
+# ********* TODO 2 **********
+# Solve coefficients
+# Use np.linalg.lstsq
+# Put coefficients to variable "theta" which we use below.
+theta, a,b,c= np.linalg.lstsq(H,z,rcond=None)
+# Predict
+z_pred = H @ theta
+Z_pred = np.reshape(z_pred, X.shape)
+
+# Subtract & show
+S = Z - Z_pred
+plt.imshow(S, cmap = 'gray')
+plt.show()
 
